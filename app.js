2b7e08c062cbe04d2b0641d6297a7f08475f13aa
mozahidsani <mozahidwebmin11d10@gmail.com>
2022-09-17 20:57:50 +0600

    schema design

--- app.js
+++ app.js
@@ -10,6 +10,94 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+// Schema............................................
+const productSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'please write product'],
+        trim:true,
+        unique:[true,'name must be uniq'],
+        minLength:[3, "name must be 3 character"],
+        maxLength:[100, "Name is too large "]
+    },
+    description:{
+      type:String,
+      required:true, 
+    },
+
+    price:{
+      type:Number,
+      required:true, 
+      min:[0, "price can be negative"],
+    },
+
+    unit:{
+      type:String,
+      required:true,
+      enum :{
+        values:["kg", "litre","pcs"],
+        massage:"user can not be {VALUE}, must be kg/litre/pcs"
+      }
+    },
+
+    Quantity:{
+        type:Number,
+        required:true, 
+        min:[0, "quantity can be negative"],
+        validate:{
+            validator:(value)=>{
+             const isInteger=Number.isInteger(value);
+             if(isInteger){
+              return true;
+             }
+             else{
+               return false;
+             }
+            }
+        },
+        message :"quantity must be a an integer"
+      },
+
+
+      status:{
+        type:String,
+        enum :{
+            values:["is stock", "out-stock", "discontinued"],
+            massage:"status can be {VALUE}"
+          }
+      },
+
+
+
+      supplier:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Supplier"
+      },
+
+      catagories:[
+        {
+            name:String,
+            required:true,
+        },
+        {
+            _id:mongoose.Schema.Types.ObjectId, 
+        }
+
+      ]
+
+
+
+//   createdAt:{
+//     type:Date,
+//     default:date.now,
+//   } ,
+
+//   updateAt:{
+//     type:Date,
+//     default:date.now,
+//   }    
+},{timestamps:true})
+// Schema.............................................................
 
 
 
--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const colors=require("colors")
 const app=require("./app")
 
 
-
 mongoose.connect(process.env. DATABASE_LOCAL).then(()=>{
 console.log(`database connected successful`.red.bold);
 })
 
 
 const  port =process.env.PORT|| 8080;
-
-
 app.listen(port,()=>{
     console.log(`App is running on port ${port}`.yellow.bold);
 })
